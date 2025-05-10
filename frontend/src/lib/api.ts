f793a66427b444baefdc7a56f51bd66892d4d1cd
neilh44 <nilesh.hanotia@outlook.com>
2025-05-10 11:12:19 +0530

    update base url

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: 'https://deedsure.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
